0599154098c10163a3864610e97e777796be8d1b
IKpassah <vanya2akak@gmail.com>
2021-08-04 00:03:47 +0300

    Seeder is good

--- API/Data/Seed/Seeder.cs
+++ API/Data/Seed/Seeder.cs
@@ -11,29 +11,66 @@ namespace API.Data.Seed
 {
     public class Seeder
     {
-        public static async Task SeedData(DataContext dataContext)
-        {
-            await SeedCategories(dataContext);
-        }
-        public static async Task SeedCategories(DataContext dataContext)
-        {
-            if(await dataContext.Users.AnyAsync()) return;
+        readonly string folderPath = "Data/Seed/";
 
-            var categoryData = await File.ReadAllTextAsync("Data/Seed/CategorySeedData.json");
-            var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
-            if(categories == null) return;
+        public async Task SeedData(DataContext dataContext)
+        {
+            //await SeedCategories(dataContext);
 
+            var categories = await SeedEntities<Category>(dataContext, "CategorySeedData.json");
             foreach(var category in categories){
                 if(category.ParentCategoryId != null)
                     category.ParentCategory = categories.FirstOrDefault(c => c.Id == category.ParentCategoryId);
             }
 
-            foreach(var category in categories){
-                dataContext.Categories.Add(category);
-            }
+
+            await SeedEntities<Product>(dataContext, "ProductSeedData.json");
 
             await dataContext.SaveChangesAsync();
         }
+
+        
+        // public  async Task SeedCategories(DataContext dataContext)
+        // {
+        //     if(await dataContext.Categories.AnyAsync()) return;
+
+        //     var categoryData = await File.ReadAllTextAsync(folderPath + "CategorySeedData.json");
+        //     var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
+        //     if(categories == null) return;
+
+            
+
+        //     foreach(var category in categories){
+        //         dataContext.Categories.Add(category);
+        //     }
+
+        //     //await dataContext.SaveChangesAsync();
+        // }
+        
+        private async Task<List<T>> GetObjectsFromJson<T>(string fileName)
+        {
+            var data = await File.ReadAllTextAsync(folderPath + fileName);
+            return JsonSerializer.Deserialize<List<T>>(data);
+        }
+
+        public async Task<List<T>> SeedEntities<T>(DataContext dataContext, string fileName) where T : class
+        {
+            var dbSet = dataContext.Set<T>();
+
+            var objects = new List<T>();
+            if(await dbSet.AnyAsync()) return objects;
+
+            objects = await GetObjectsFromJson<T>(fileName);
+            if(objects.Count() == 0) return objects;
+
+            foreach(var product in objects){
+                dbSet.Add(product);
+            }
+
+            return objects;
+        }
+
+        
 
         // public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager){
         //     if(await userManager.Users.AnyAsync()) return;
