da812dadae72f1edd985c767df53cdfea5932201
IKpassah <vanya2akak@gmail.com>
2021-08-12 15:39:31 +0300

    account controller register works

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 
 
+using System;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
@@ -52,12 +54,15 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Email)) return BadRequest("User Name is taken");
+            if (await UserExists(registerDto.Email)) return BadRequest("Email is taken");
 
             var user = _mapper.Map<AppUser>(registerDto);
 
+            if(!IsValidEmail(registerDto.Email)) return BadRequest("Wrong email");
 
             user.Email = registerDto.Email.ToLower();
+            var emailParts = registerDto.Email.ToLower().Split('@');
+            user.UserName = emailParts[0] + emailParts[1];
 
 
 
@@ -85,6 +90,19 @@ namespace API.Controllers
             return await _accountService.Users.AnyAsync(u => u.Email == email.ToLower());
         }
 
+        private bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
 
     }
 }
