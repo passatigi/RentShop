5a5c845f70a17ce9e1b8e6f9072a564adce34144
antonpby <anton.38.anton.38@gmail.com>
2021-08-19 02:02:22 +0300

    Added product service

--- client/src/app/_services/products.service.ts
+++ client/src/app/_services/products.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { identifierModuleUrl } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from '../_models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getProductsByCaregoryId(id: number){
+    let httpParams = new HttpParams().set('CategoryId', id);
+    return this.http.get<Product[]>(this.baseUrl + 'Product', {params: httpParams});
+  }
+
+  getProductById(id: number){
+    console.log('qqq');
+    
+    return this.http.get<Product>(this.baseUrl + 'Product/detail/' + id)
+  }
 }
