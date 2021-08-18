723d61c72e08aba5e9f9d05aff1681f78491e650
antonpby <anton.38.anton.38@gmail.com>
2021-08-19 02:01:41 +0300

    Added detailed product page

--- client/src/app/_products/product-detail/product-detail.component.ts
+++ client/src/app/_products/product-detail/product-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/_models/product';
+import { ProductsService } from 'src/app/_services/products.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  id: number;
+  product: Product;
 
-  constructor() { }
+  constructor(private productService: ProductsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe( params => {
+      this.id = params['id'];
+    })
+
+    this.loadProduct(this.id);
+
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '600px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
   }
 
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photoUrl of this.product.productImgsLinks){
+      imageUrls.push({
+        small: photoUrl,
+        medium: photoUrl,
+        big: photoUrl
+      })
+    }
+    return imageUrls;
+  }
+
+  loadProduct(id: number){
+    console.log(id);
+    this.productService.getProductById(id).subscribe(product =>{
+      this.product = product;
+      this.galleryImages = this.getImages();
+    });
+
+  }
 }
