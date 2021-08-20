84ba27bf31ac8ecc7ad8ee93ec5ab31a757a08bd
antonpby <anton.38.anton.38@gmail.com>
2021-08-20 15:40:14 +0300

    Improved main page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,11 +8,13 @@ import { MainComponent } from './main/main.component';
 import { AdminGuard } from './_guards/admin.guard';
 import { ProductDetailComponent } from './_products/product-detail/product-detail.component';
 import { ProductListComponent } from './_products/product-list/product-list.component';
+import { CartDetaisComponent } from './_cart/cart-detais/cart-detais.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'product', component: ProductDetailComponent},
   {path: 'category', component: ProductListComponent},
+  {path: 'cart', component: CartDetaisComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
   {path: 'admin', component: RegistrationComponent},
