184d33b8b47c3d07641c3e3b338bf81d662942e3
antonpby <anton.38.anton.38@gmail.com>
2021-08-30 12:38:54 +0300

    Created OrderController and OrderDto entity

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,44 +9,55 @@ namespace API.Helpers
     {
         public AutoMapperProfiles()
         {
-               CreateMap<Category, CategoryDto>();
+            CreateMap<Category, CategoryDto>();
 
-               CreateMap<RegisterDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
 
-               CreateMap<Product, ProductDto>()
+            CreateMap<Product, ProductDto>()
                 .ForMember(dest => dest.imgUrl, opt => opt.MapFrom(p => p.ProductImgs.FirstOrDefault().Link));
 
-                CreateMap<Product, DetailedProductDto>()
+            CreateMap<Product, DetailedProductDto>()
                 .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(p => p.Category.Name))
                 .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(p => p.Category.Id))
                 .ForMember(dest => dest.ProductImgsLinks, opt => opt.MapFrom(p => p.ProductImgs.Select(p => p.Link)));
 
             CreateMap<ProductFeature, ProductFeatureDto>()
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(f => f.Feature.Name))
-            .ForMember(dest => dest.Explanation, opt => opt.MapFrom(f => f.Feature.Explanation))
-            .ForMember(dest => dest.GroupName, opt => opt.MapFrom(f => f.Feature.GroupName))
-            .ForMember(dest => dest.FeatureId, opt => opt.MapFrom(f => f.Feature.Id));
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(f => f.Feature.Name))
+                .ForMember(dest => dest.Explanation, opt => opt.MapFrom(f => f.Feature.Explanation))
+                .ForMember(dest => dest.GroupName, opt => opt.MapFrom(f => f.Feature.GroupName))
+                .ForMember(dest => dest.FeatureId, opt => opt.MapFrom(f => f.Feature.Id));
 
             CreateMap<RealProduct, RealProductDto>();
             CreateMap<RealProductDto, RealProduct>();
 
             CreateMap<RealProduct, RealProductSchedule>()
-            .ForMember(dest => dest.RealProductId, opt => opt.MapFrom(p => p.Id))
-            .ForMember(dest => dest.Segments, opt => opt.MapFrom(p => p.OrderProducts.Select(op => op.Order)
-                        .Select(o => new ProductScheduleSegmentsDto 
-                        { 
-                            RentStart = o.RequiredDate,
-                            RentEnd = o.RequiredReturnDate 
-                        })));
+                .ForMember(dest => dest.RealProductId, opt => opt.MapFrom(p => p.Id))
+                .ForMember(dest => dest.Segments, opt => opt.MapFrom(p => p.OrderProducts.Select(op => op.Order)
+                .Select(o => new ProductScheduleSegmentsDto 
+                { 
+                    RentStart = o.RequiredDate,
+                    RentEnd = o.RequiredReturnDate 
+                })));
             
             CreateMap<AddProductDto, Product>();
 
             CreateMap<Feature, FeatureDto>()
-            .ForMember(dest => dest.FeatureId, opt => opt.MapFrom(f => f.Id));
+                .ForMember(dest => dest.FeatureId, opt => opt.MapFrom(f => f.Id));
 
             CreateMap<AddFeatureDto, Feature>();
 
             CreateMap<AddProductFeatureDto, ProductFeature>();
+
+            CreateMap<OrderDto, Order>();
+
+            CreateMap<Order, OrderDto>();
+
+            CreateMap<OrderProduct, RealProductDto>()
+                // .ForMember(dest => dest.Id, opt => opt.MapFrom(f => f.RealProduct.Id))
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(f => f.RealProduct.ProductId))
+                .ForMember(dest => dest.SerialNumber, opt => opt.MapFrom(f => f.RealProduct.SerialNumber))
+                .ForMember(dest => dest.Condition, opt => opt.MapFrom(f => f.RealProduct.Condition))
+                .ForMember(dest => dest.RentPrice, opt => opt.MapFrom(f => f.RealProduct.RentPrice));
         }
     }
 }
