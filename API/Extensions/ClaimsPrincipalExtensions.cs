ba47d03bbe5b71d8a2b26e96c60dd0cacc49f19e
Logoshin Nikolai <lgrf53@gmail.com>
2021-09-02 12:34:33 +0300

    another fix, added getEmail extension

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -9,10 +9,13 @@ namespace API.Extensions
              return user.FindFirst(ClaimTypes.Name)?.Value;
         }
 
-         public static int GetUserId(this ClaimsPrincipal user)
+        public static int GetUserId(this ClaimsPrincipal user)
         {
              return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value) ;
         }
 
+        public static string GetEmail(this ClaimsPrincipal user){
+            return user.FindFirst(ClaimTypes.Email)?.Value;
+        }
     }
 }
