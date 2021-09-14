d970b4ec6ccd81aeab10823706b994a524be3aa0
antonpby <anton.38.anton.38@gmail.com>
2021-09-14 16:16:46 +0300

    Changed cards in the cart

--- client/src/app/_cart/card-item/card-item.component.ts
+++ client/src/app/_cart/card-item/card-item.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit, Self} from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, NgControl, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Address } from 'src/app/_models/address';
 import { CartItem } from 'src/app/_models/cartItem';
 import { CreateOrder } from 'src/app/_models/createOrder';
 import { OrderProduct } from 'src/app/_models/orderProduct';
 import { Segment } from 'src/app/_models/segment';
+import { AccountService } from 'src/app/_services/account.service';
 import { DateService } from 'src/app/_services/date.service';
 import { OrderService } from 'src/app/_services/order.service';
 import { SelectProductService } from 'src/app/_services/select-product.service';
@@ -12,6 +14,7 @@ import { SelectProductService } from 'src/app/_services/select-product.service';
   selector: 'app-card-item',
   templateUrl: './card-item.component.html',
   styleUrls: ['./card-item.component.css']
+
 })
 export class CardItemComponent implements OnInit {
 
@@ -19,18 +22,21 @@ export class CardItemComponent implements OnInit {
   disabledDates: Date[] = [];
   minDate: Date;
   tomorrow: Date;
-  public show: boolean = true;
+  show: boolean = true;
   days: Segment[];
   myForm: FormGroup;
   isCollapsed = true;
   error: string;
   daterangepickerModel?: Date[];
+  addresses: Address[];
+  selectedValue: Address;
 
   @Input() item: CartItem;
   constructor(private selectProductService: SelectProductService, 
     private dateService: DateService,
     private formBuilder: FormBuilder,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private accountService: AccountService
     ) {}
 
   ngOnInit(): void {
@@ -41,10 +47,19 @@ export class CardItemComponent implements OnInit {
     });
 
     this.myForm = this.formBuilder.group({
-      shippedAdress: ['', Validators.required],
-      returnAdress: ['', Validators.required],
+      shippedAddress: ['', Validators.required],
+      returnAddress: ['', Validators.required],
       comments: ['', ],
       range: ['', [Validators.required, this.matchRangeValues()]]
+    });
+
+    this.getUserAddresses();
+  }
+
+  getUserAddresses(){
+    let user = JSON.parse(localStorage.getItem('user'));
+    this.accountService.getUserAddresses(user.email).subscribe(addresses => {
+      this.addresses = addresses;
     });
   }
 
@@ -52,6 +67,11 @@ export class CardItemComponent implements OnInit {
     return (control: AbstractControl) => {
       let requiredReturnDate = new Date(control.value[1]);
       let requiredDate = new Date(control.value[0]);
+      
+      if (requiredDate.getDate() == requiredReturnDate.getDate()){
+        this.error = 'You cannot rent an item for 0 days';
+        return { invalidRangeOfDays: 'You cannot rent an item for 0 days'}
+      }
 
       for (var i = 0; i < this.disabledDates.length; i++)
         {
@@ -66,63 +86,46 @@ export class CardItemComponent implements OnInit {
     }
   }
 
-  // add subscribe
-  addOrder(){
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.myForm.controls[controlName];
     
-    let user = JSON.parse(localStorage.getItem('user'));
+    const result = control.invalid && control.touched;
+    
+    return result;
+    }
+
+  countDays(): number{
+    let start = new Date(this.myForm.controls.range.value[0]);
+    let end =  new Date(this.myForm.controls.range.value[1]);
+    var diff = Math.abs(start.getTime() - end.getTime());
+    return Math.ceil(diff / (1000 * 3600 * 24));
+  }
+
+  daysIsNaN(numb: number){
+    return Number.isNaN(numb)
+  }
+
+  addOrder(){
     let orderProducts: OrderProduct[] = [];
     let orderProduct: OrderProduct = {
       realProductId: this.item.id
     }
     orderProducts.push(orderProduct);
 
-    // let newOrder: CreateOrder = {
-    //   orderProducts : orderProducts,
-    //   requiredDate : this.myForm?.controls.requiredDate.value,
-    //   requiredReturnDate : this.myForm?.controls.requiredReturnDate.value,
-    //   comments : this.myForm?.controls.comments?.value,
-    //   customeId : user.id,
-    //   shippedAdress : this.myForm?.controls.shippedAdress.value,
-    //   returnAdress : this.myForm?.controls.returnAdress.value,
-    //   totalPrice = orderProduct.realProduct.rentPrice * 
-    // }
-
-    // this.orderService.addOrder(newOrder);
-
-    this.remove();
-  }
-
-
-  matchValues(matchTo: string): ValidatorFn{
-    return (control: AbstractControl) => {
-      let requiredReturnDate = new Date();
-      let requiredDate = new Date();
-
-      if ( matchTo == 'requiredDate')
-      {
-        requiredReturnDate = new Date(control?.value);
-        requiredDate = new Date(control?.parent?.controls[matchTo].value);
-      }
-      else {
-        requiredReturnDate = new Date(control?.parent?.controls[matchTo].value);
-        requiredDate = new Date(control?.value);
-      }
-
-      for (var i = 0; i < this.disabledDates.length; i++)
-        {
-          if ( new Date(this.disabledDates[i]) >= requiredDate && new Date(this.disabledDates[i]) <= requiredReturnDate)
-            {
-              this.error = 'You can only rent the item on available days';
-              return { invalidRangeOfDays: 'You can only rent the item on available days'}
-            }
-        }
-      if (requiredDate >= requiredReturnDate){
-        this.error = 'start after end';
-        return { invalidRangeOfDays: 'start after end'}
-      }
-      this.error = undefined;
-      return null;
+    let newOrder: CreateOrder = {
+      orderProducts : orderProducts,
+      requiredDate : this.myForm?.controls.range.value[0],
+      requiredReturnDate : this.myForm?.controls.range.value[1],
+      comments : this.myForm?.controls.comments?.value,
+      customerId : this.myForm?.controls.shippedAddress.value.appUserId,
+      shippedAddressId : this.myForm?.controls.shippedAddress.value.id,
+      returnAddressId : this.myForm?.controls.returnAddress.value.id,
+      totalPrice : this.item.rentPrice * this.countDays()
     }
+
+    this.orderService.addOrder(newOrder);
+    this.remove();
   }
 
   addDisabledDays(){
